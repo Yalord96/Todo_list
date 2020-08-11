42207ae37aca8e09c4b1b6bb5690cf5c3959111e
Yalord96 <sergeynosenko96@gmail.com>
2020-08-11 21:13:40 +0300

    edit, delete, clearList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,10 +1,25 @@
 import React, {Component} from 'react';
-import TodoItem from "./TodoItem";
+import TodoItem from "../TodoItem";
+import './TodoList.css';
+
 export default class TodoList extends Component {
     render() {
+        const {items, clearList, handleDelete, handleEdit} = this.props
         return (
-            <ul className={"list-group my-5"}>
-                <button type="button" className="btn btn-primary">Clear items</button>
+            <ul className="list-group my-5 d-flex align-items-center">
+                {
+                    items.map(item => {
+                        return (
+                            <TodoItem key={item.id}
+                                      title={item.title}
+                                      handleDelete={()=>handleDelete(item.id)}
+                                          handleEdit={()=>handleEdit(item.id)}
+                            />
+                        );
+                    })
+                }
+                <button type="button" className="todo_list btn btn-primary"
+                onClick={clearList}>Clear items</button>
             </ul>
         );
     }
